614fe2c08c80f1c458d0782046c99ca1dae437d4
Ceyhun-gl <ceyhungul148@gmail.com>
2024-01-15 22:30:40 +0300

    images changed

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,26 +1,26 @@
 export const sliderImageList = [
   {
-    img: 'https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.pexels.com/photos/10639763/pexels-photo-10639763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     id: 1,
   },
   {
-    img: 'https://images.pexels.com/photos/2859169/pexels-photo-2859169.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.pexels.com/photos/10639763/pexels-photo-10639763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     id: 2,
   },
   {
-    img: 'https://images.pexels.com/photos/2902747/pexels-photo-2902747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.pexels.com/photos/10639763/pexels-photo-10639763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     id: 3,
   },
   {
-    img: 'https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.pexels.com/photos/10639763/pexels-photo-10639763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     id: 4,
   },
   {
-    img: 'https://images.pexels.com/photos/2859169/pexels-photo-2859169.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.pexels.com/photos/10639763/pexels-photo-10639763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     id: 5,
   },
   {
-    img: 'https://images.pexels.com/photos/2902747/pexels-photo-2902747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    img: 'https://images.pexels.com/photos/10639763/pexels-photo-10639763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     id: 6,
   },
 ];
