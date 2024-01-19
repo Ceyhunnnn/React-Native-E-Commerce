dbdfc5a73879bf9fde1b2299c22932d70cccd4ed
Ceyhun-gl <ceyhungul148@gmail.com>
2024-01-19 19:45:35 +0300

    formik and yup installed, atuh types created

---
+++ src/types/auth.ts
@@ -0,0 +1,10 @@
+export interface ILoginType {
+  email: string;
+  password: string;
+}
+export interface IRegisterType {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
